34e7afc61e1cf0b9ea6691682ea3ce43abbcb666
Steffen Buchholz <steffen.buchholz@gmx.de>
2024-12-21 20:08:42 +0100

    Translation and Movement

--- Assets/Scripts/Translate.cs
+++ Assets/Scripts/Translate.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class Translate : MonoBehaviour
 {
+    // Geschwindigkeit der Bewegung
+    public float speed = 5.0f;
+
+    // Rotationsgeschwindigkeit
+    public float rotationSpeed = 50.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +17,10 @@ public class Translate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Bewegung des Objekts entlang der Z-Achse
+        transform.Translate(0, 0, speed * Time.deltaTime, Space.World);
+
+        // Rotation des Objekts um die Y-Achse
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
     }
 }
