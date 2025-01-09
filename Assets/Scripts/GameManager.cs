f817c2bc4d491d2ecb570236e0870156d87e734b
Steffen Buchholz <steffen.buchholz@gmx.de>
2025-01-10 00:28:55 +0100

    feat: MainMenu, Mercury

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,11 +4,13 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject _pauseMenu = null;
-    public GameObject _startMenu = null;
+    public GameObject _Menu = null;
     public int targetFrameRate = 60; // Ziel-Framerate
 
-    private bool _isPaused = true;
+    [SerializeField]
+    private bool _hasPauseMenu = true;
+
+    private bool _isPaused = false;
     public bool IsPaused => _isPaused; // Öffentliche Eigenschaft, um den Pausenstatus abzurufen
 
 
@@ -18,49 +20,49 @@ public class GameManager : MonoBehaviour
     {
         Application.targetFrameRate = targetFrameRate; // Setzt die Ziel-Framerate
         cancelAction = InputSystem.actions.FindAction("Cancel");
-        _pauseMenu.SetActive(false);
-        _startMenu.SetActive(true);
-        Time.timeScale = 0;
+        setPause(!_hasPauseMenu);
     }
 
     public void Update()
     {
-        if (cancelAction.WasPerformedThisFrame())
+        if (cancelAction.WasPerformedThisFrame() && _hasPauseMenu)
         {
-            if ( _isPaused)
-            {
-                Time.timeScale = 1;
-                _pauseMenu.SetActive(false);
-                Cursor.lockState = CursorLockMode.Locked; // Sperrt den Cursor in der Mitte des Bildschirms
-            }
-            else
-            {
-                Time.timeScale = 0;
-                _pauseMenu.SetActive(true);
-                Cursor.lockState = CursorLockMode.None; // Löst den Cursor vom Bildschirm
-            }
+            setPause(!_isPaused);
             _isPaused = !_isPaused;
         }
     }
-    public void OnStartButtonClick()
+    public void OnMainMenuButtonClick()
     {
-        _isPaused = false;
-        _startMenu.SetActive(false);
-        Cursor.lockState = CursorLockMode.Locked;
-        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
     }
     public void OnExitButtonClick()
     {
         Application.Quit();
     }
 
-    public void OnLevel0ButtonClick()
-    {
-        SceneManager.LoadScene(0);
-    }
-
     public void OnLevel1ButtonClick()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void OnLevel2ButtonClick()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    private void setPause(bool pause)
+    {
+        if (pause)
+        {
+            Time.timeScale = 0;
+            _Menu.SetActive(true);
+            Cursor.lockState = CursorLockMode.None; // Löst den Cursor vom Bildschirm
+        } else
+        {
+            Time.timeScale = 1;
+            _Menu.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked; // Sperrt den Cursor in der Mitte des Bildschirms
+        }
+
     }
 }
