c295d5b781a7698f7e006506fc48e9c3a90eac63
Steffen Buchholz <56304703+buchholzs@users.noreply.github.com>
2025-12-06 18:10:59 +0100

    Joystick Pack, Icons, Dropship

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,16 @@
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using static UnityEngine.Rendering.STP;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject _Menu = null;
+    public GameObject _ToggleMobile = null;
     public int targetFrameRate = 60; // Ziel-Framerate
+    public static bool isMobile = Application.platform == RuntimePlatform.Android;
 
     [SerializeField]
     private bool _hasPauseMenu = true;
@@ -21,9 +26,18 @@ public class GameManager : MonoBehaviour
         Application.targetFrameRate = targetFrameRate; // Setzt die Ziel-Framerate
         cancelAction = InputSystem.actions.FindAction("Cancel");
         setPause(!_hasPauseMenu);
+        if (_ToggleMobile != null)
+        {
+            Toggle _mobileToggle = _ToggleMobile.GetComponent<Toggle>();
+            if (_mobileToggle != null)
+            {
+                // aktuellen globalen Wert setzen
+                _mobileToggle.isOn = isMobile;
+            }
+        }
     }
 
-    public void Update()
+public void Update()
     {
         if (cancelAction.WasPerformedThisFrame() && _hasPauseMenu)
         {
@@ -49,6 +63,18 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene(2);
     }
+    public void OnMainMenuToggleClick()
+    {
+        if (_ToggleMobile != null)
+        {
+            Toggle _mobileToggle = _ToggleMobile.GetComponent<Toggle>();
+            if (_mobileToggle != null)
+            {
+                // aktuellen globalen Wert setzen
+                isMobile = _mobileToggle.isOn;
+            }
+        }
+    }
 
     private void setPause(bool pause)
     {
@@ -56,13 +82,19 @@ public class GameManager : MonoBehaviour
         {
             Time.timeScale = 0;
             _Menu.SetActive(true);
-            Cursor.lockState = CursorLockMode.None; // Löst den Cursor vom Bildschirm
-        } else
+            if (!isMobile)
+            {
+                Cursor.lockState = CursorLockMode.None; // Löst den Cursor vom Bildschirm
+            }
+        }
+        else
         {
             Time.timeScale = 1;
             _Menu.SetActive(false);
-            Cursor.lockState = CursorLockMode.Locked; // Sperrt den Cursor in der Mitte des Bildschirms
+            if (!isMobile)
+            {
+                Cursor.lockState = CursorLockMode.Locked; // Sperrt den Cursor in der Mitte des Bildschirms
+            }
         }
-
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,10 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speed = 2.5f;
     [SerializeField] private float _mouseSensitivity = 3.0f;
+    [SerializeField] private float _joystickSensitivity = 3.0f;
     public GameManager _gameManager = null;
+    public Joystick _joystickLeft = null;
+    public Joystick _joystickRight = null;
 
     private float _horizontal = 0.0f;
     private float _vertical = 0.0f;
@@ -22,6 +25,8 @@ public class PlayerController : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody>();
         _xRotation = _rigidbody.transform.localEulerAngles.x;
         _yRotation = _rigidbody.transform.localEulerAngles.y;
+        _joystickLeft.gameObject.SetActive(GameManager.isMobile);
+        _joystickRight.gameObject.SetActive(GameManager.isMobile);
     }
 
     void Start()
@@ -34,14 +39,26 @@ public class PlayerController : MonoBehaviour
     {
         if (!_gameManager.IsPaused)
         {
-            Vector2 moveValue = moveAction.ReadValue<Vector2>();
-            Vector2 lookValue = lookAction.ReadValue<Vector2>();
+            Vector2 moveValue = Vector2.zero;
+            Vector2 lookValue = Vector2.zero;
+            if (GameManager.isMobile)
+            {
+                _horizontal = _joystickLeft.Horizontal;
+                _vertical = _joystickLeft.Vertical;
+                _mouseX = _joystickRight.Horizontal * _joystickSensitivity * Time.deltaTime;
+                _mouseY = _joystickRight.Vertical * _joystickSensitivity * Time.deltaTime;
+            }
+            else
+            {
+                moveValue = moveAction.ReadValue<Vector2>();
+                lookValue = lookAction.ReadValue<Vector2>();
+                _horizontal = moveValue.x;
+                _vertical = moveValue.y;
+                _mouseX = lookValue.x * _mouseSensitivity * Time.deltaTime;
+                _mouseY = lookValue.y * _mouseSensitivity * Time.deltaTime;
+            }
 
-            _horizontal = moveValue.x;
-            _vertical = moveValue.y;
 
-            _mouseX = lookValue.x * _mouseSensitivity * Time.deltaTime;
-            _mouseY = lookValue.y * _mouseSensitivity * Time.deltaTime;
 
             _xRotation -= _mouseY;
             _xRotation = Mathf.Clamp(_xRotation, -90f, 90f); // Begrenzung der vertikalen Rotation
